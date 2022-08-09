349ad0429631b7ef45c58ca0f31b5a194730dfe4
Adriano Ferreira <adriano.13lp@gmail.com>
2022-08-09 18:58:58 -0300

    introduzindo novos bugs

--- AnalisadorSintatico/teste.py
+++ AnalisadorSintatico/teste.py
@@ -1,5 +1,17 @@
 import copy
 import pandas as pd
+import AnalisadorLexico.main
+
+afd = main.AFD_LEX()
+afd.mgol_trans()
+listaTokens = []
+simbolos = TabelaSimbolos()
+linha = 1
+coluna = 1
+auxArq = 0
+auxFimArq = False
+isToken = False
+erroLexico = False
 
 actions = pd.read_csv('Actions.csv')
 goto = pd.read_csv('goto.csv')
@@ -9,3 +21,11 @@ with open('gramatica-Mgol.txt') as file:
     gramatica = {regra: file.readline().replace('\n','') for regra in range(1,40)}
 
 print(gramatica[1])
+
+
+def lr_parser(actions, goto, pilha):
+    pilha = [0]
+    estado = pilha[-1]
+    a = pilha.peek()
+    while True:
+        acao = actions[a][s]
