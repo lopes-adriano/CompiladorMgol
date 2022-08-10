5d8b18755104ec9a145cdd435913f046ab24be6c
Gborges42 <45388282+Gborges42@users.noreply.github.com>
2022-08-09 23:35:13 -0300

    automato do parser

--- teste.py
+++ teste.py
@@ -1,4 +1,5 @@
 import copy
+from numpy import append
 import pandas as pd
 from AnalisadorLexico import afd, classToken, util, tabelaDeSimbolos
 
@@ -24,24 +25,49 @@ with open('gramatica-Mgol.txt') as file:
 with open("FONTE.alg", "r") as f:
     arquivo = f.read()
 
-print(gramatica[1])
-
-ip = "ES"
-stack = 9
-
-print(goto[ip][stack])
-
-print(gramatica[6])
-
-
+def acao(acao, a, pilha):
+    estado = ""
+    ant_est = 0
+    gram = ""
+    nregra = ""
+    if(acao[0] == "s"):
+        pilha.append(a)
+        for c in range(1, len(acao)):
+            estado += acao[c]
+        pilha.append(int(estado))
+        return pilha
+    if(acao[0] == "r"):
+        for c in range(1, len(acao)):
+            nregra += acao[c]
+        gram = gramatica[int(nregra)]
+        gram = gram.split(" ")
+        for c in range(0, (len(gram)-2)*2):
+            pilha.pop()
+        ant_est = pilha[len(pilha)-1]
+        ant_est = goto[gram[0]][int(ant_est)]
+        pilha.append(gram[0])
+        pilha.append(ant_est)
+        return pilha
 
 def lr_parser(actions, goto):
     pilha = [0]
-    estado = pilha[-1]
-    a = util.scanner(arquivo).classe
-    print(a)
-    
-    print(actions[a][estado])
+    estado = 0
+    while(True):
+
+        estado = pilha[len(pilha)-1]
+
+        a = util.scanner(arquivo).classe
+        print(a)
+        print(estado)
+        print(actions[a][estado])
+        pilha = acao(actions[a][estado], a, pilha)
+        print(pilha)
+
+
 
 lr_parser(actions, goto)
 
+def testeBruto():
+    print(actions["varinicio"][2])
+
+#testeBruto()
